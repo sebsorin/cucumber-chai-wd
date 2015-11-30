18d7e174a091af9032dd4df2a5736ed580590970
sebsorin <sebengsimans.sorin@gmail.com>
2015-11-30 22:33:28 +0100

    add tests

--- gulpfile.js
+++ gulpfile.js
@@ -11,31 +11,48 @@ var webdriver_update = require("gulp-protractor").webdriver_update;
 
 var child_process = require('child_process');
 
+var serverProcess;
 
 // Downloads the selenium webdriver
 gulp.task('webdriver_update', webdriver_update);
 
-// launch protractor server
-gulp.task('protractor_server', function() {
-	var server = child_process.spawn('npm',['start']);
 
-	server.stdout.on('data', function (data) {
+
+// launch protractor server
+gulp.task('protractor_server', function(done) {
+
+  // to be able to kill at end of test, run in detach
+	serverProcess = child_process.spawn('npm',['start'], {detached: true});
+
+	serverProcess.stdout.on('data', function (data) {
+  		console.log('' + data);
+  		if (data.toString().includes('Starting express web server')) {
+  			done();
+  		}
+	});
+	serverProcess.stderr.on('data', function (data) {
   		console.log('' + data);
 	});
-	server.stderr.on('data', function (data) {
-  		console.log('' + data);
+	serverProcess.stderr.on('exit', function () {
+  		done('Error launching server');
 	});
 });
 
 
-
+// launch protractor test server before e2e testS
 gulp.task('e2e',['webdriver_update','protractor_server'], function(done){
-	gulp.src('src/test/cucumber/features/*.feature')
+	gulp.src('test/cucumber/features/*.feature')
 		.pipe(protractor({
-		//debug: true,
+		    //debug: true,
         configFile: "test/protractor.conf.js",
-        args: ['--baseUrl', 'http://127.0.0.1:8080']
+        args: ['--baseUrl', 'http://127.0.0.1:8081']
     })).on('error', function(e) {
-        console.log(e)
-    }).on('end', done);  
+        console.log(e);
+        console.log('#### KILLING SERVER ######');
+        process.kill(-serverProcess.pid);
+        done(e);
+    }).on('end', function() {
+    	 process.kill(-serverProcess.pid);
+    	done();
+    });  
 })
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -5,17 +5,11 @@ var fs = require('fs');
 
 exports.config = {
   framework: 'cucumber',
-  specs: [
-    'features/*.feature'
-  ],
   capabilities: {
     browserName: 'firefox'
   },
   cucumberOpts: {
-    require: ['cucumber/steps/*.js','cucumber/support/*.js'],
-    format: 'json',
-    logTo: function(data) {
-    	fs.appendFile('toto',data)
-    } 
+    require: ['cucumber/support.js','cucumber/steps/*.js'],
+    format: 'json'
   }
 }
